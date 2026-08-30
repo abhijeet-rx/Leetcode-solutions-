7a5b864e9278d3bb3bffe864aa09f7fd6c141450
Abhijeet Singh <69684226+abhijeet-rx@users.noreply.github.com>
2026-08-30 11:16:45 +0530

    Solved: Search a 2D Matrix (leetcode)

--- leetcode/Medium/search-a-2d-matrix.java
+++ leetcode/Medium/search-a-2d-matrix.java
@@ -4,28 +4,28 @@
 // Language: java
 // Verdict: Accepted
 // URL: https://leetcode.com/problems/search-a-2d-matrix/
-// Solved on: 2026-08-30T05:40:58.148Z
+// Solved on: 2026-08-30T05:46:43.790Z
 
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int row  = matrix.length;
         int col = matrix[0].length;
-        int start =0;
-        int end = row*col-1;
-        int mid  = start+(end-start)/2;
+        int start = 0;
+        int end  = row*col-1;
+        int mid = start+(end-start)/2;
         while(start<=end){
             int element = matrix[mid/col][mid%col];
+            
             if(element == target){
                 return true;
-
             }
-            else if(element>target){
-                end= mid-1;
+            else if (element>target){
+                end = mid-1;
             }
             else{
-                start  =mid+1;
+                start = mid+1;
             }
-            mid = start+(end-start)/2;
+            mid  = start+(end-start)/2;
         }
         return false;
     }
