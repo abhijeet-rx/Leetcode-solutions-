a6738899e8696b3bb09f573a58949cd411f06faf
Abhijeet Singh <69684226+abhijeet-rx@users.noreply.github.com>
2026-09-05 22:56:49 +0530

    Solved: Topological Sort (geeksforgeeks)

--- geeksforgeeks/Practice/topological-sort.java
+++ geeksforgeeks/Practice/topological-sort.java
@@ -3,45 +3,45 @@
 // Language: unknown
 // Verdict: Accepted
 // URL: https://www.geeksforgeeks.org/problems/topological-sort/1
-// Solved on: 2026-09-05T16:51:00.000Z
+// Solved on: 2026-09-05T17:26:42.512Z
 
 class Solution {
-    void topological(ArrayList<ArrayList<Integer>> adj,Stack<Integer> s,int node , boolean[] visited ){
-        visited[node] = true;
-        for(int neighbour:adj.get(node)){
-            if(!visited[neighbour]){
-                topological(adj,s,neighbour,visited);
-            }
-        }
-        s.push(node);
-    }
+    // TOPOLOGICAL SORT USING KAHN AlGO
     public ArrayList<Integer> topoSort(int V, int[][] edges) {
-        // code here
-        ArrayList<Integer> ans = new ArrayList<>();
-        boolean[] visited = new boolean[V];
-        Stack<Integer> s = new Stack<>();
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i = 0;i<V;i++){
-            adj.add(new ArrayList<>());
-            
-            
-        }
-        for(int[] edge:edges){
-            int u = edge[0];
-            int v = edge[1];
-            adj.get(u).add(v);
-            
-        }
-        for(int i =0;i<V;i++){
-            if(!visited[i]){
-                topological(adj,s,i,visited);
-            }
-        }
-        while(!s.isEmpty()){
-            ans.add(s.peek());
-            s.pop();
-        }
-        return ans;
+       ArrayList<Integer> ans = new ArrayList<>();
+       Queue<Integer> q = new LinkedList<>();
+       int[] indegree = new int[V];
+       ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+       for(int i=0;i<V;i++){
+           adj.add(new ArrayList<>());
+       }
+       for(int i = 0;i<edges.length;i++){
+           int u = edges[i][0];
+           int v = edges[i][1];
+           adj.get(u).add(v);
+           
+       }for(int i=0;i<V;i++){
+       for(int neighbour : adj.get(i)){
+           indegree[neighbour]++;
+       }
+           
+       }
+       for(int i=0;i<V;i++){
+           if(indegree[i]==0){
+               q.add(i);
+           }
+       }
+       while(!q.isEmpty()){
+           int node = q.poll();
+           ans.add(node);
+           for(int neighbour:adj.get(node)){
+               indegree[neighbour]--;
+               if(indegree[neighbour]==0){
+                   q.add(neighbour);
+               }
+           }
+       }
+       return ans;
         
     }
 }
